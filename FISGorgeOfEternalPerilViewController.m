cda69f153a87beb763905b4bf2e2790bc7818d43
rebeccabartels <rebeccakbartels@icloud.com>
2016-06-27 09:55:13 -0400

    Done.

--- FISGorgeOfEternalPerilViewController.m
+++ FISGorgeOfEternalPerilViewController.m
@@ -2,6 +2,17 @@
 
 #import "FISGorgeOfEternalPerilViewController.h"
 
+
+
 @implementation FISGorgeOfEternalPerilViewController
+
+
+
+
+- (IBAction)acceptMyFateTapped:(id)sender{
+    [self dismissViewControllerAnimated:YES completion:nil];
+    NSLog(@"I'm being clicked"); 
+}
+
 
 @end
